b156b353cb4c792a044245ded59bb9eb4561e3a7
prshanilka <ramesh.shanilka@gmail.com>
2024-04-10 12:01:30 +0530

    remove unwanted files

--- src/main.tsx
+++ src/main.tsx
@@ -3,24 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "@emotion/react";
 import { CssBaseline } from "@mui/material";
-
-import { createTheme } from "@mui/material/styles";
-import { red } from "@mui/material/colors";
-
-// A custom theme for this app
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#556cd6",
-    },
-    secondary: {
-      main: "#19857b",
-    },
-    error: {
-      main: red.A400,
-    },
-  },
-});
+import theme from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
